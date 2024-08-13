e600d586ca4e56dfcf9de59aac89500ea3adaa78
Sanjyrana <Sanjayrana5113@gmail.com>
2024-08-13 15:18:10 +0530

    made sidebar menu

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let close = document.querySelector(".nav-part2-sidebar > i")
 
 h1.addEventListener("click",() =>{
     sidebar.classList.add("sidebar")
+    
 })
 
 close.addEventListener("click",() =>{
